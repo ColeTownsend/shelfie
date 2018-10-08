669fd98c06488cde252e57c1f910bc6fab24c9bb
Cole Townsend <cole@coletownsend.com>
2018-10-08 09:19:37 -0400

    init

---
+++ src/utils/siteConfig.js
@@ -0,0 +1,21 @@
+module.exports = {
+  siteTitle: 'Shelfie',
+  siteTitleAlt: 'Shelfie - Monetize untapped shelf space', // This allows an alternative site title for SEO schema.
+  publisher: 'Shelfie', // Organization name used for SEO schema
+  siteDescription:
+    'Make your guests feel at home! With Shelfie you can provide your guests with all the snacks and extras they want. Do more with your untapped physical shelf space',
+  siteUrl: 'https://shelfie.netlify.com', // Site domain. Do not include a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
+  postsPerHomePage: 7, // Number of posts shown on the 1st page of of the index.js template (home page)
+  postsPerPage: 6, // Number of posts shown on paginated pages
+  author: 'Shelfie', // Author for RSS author segment and SEO schema
+  authorUrl: 'https://shelfie.co/about/', // URL used for author and publisher schema, can be a social profile or other personal site
+  userTwitter: '@getshelfie', // Change for Twitter Cards
+  shortTitle: 'Shelfie', // Used for App manifest e.g. Mobile Home Screen
+  shareImage: '/logos/share.jpg', // Open Graph Default Share Image. 1200x1200 is recommended
+  shareImageWidth: 900, // Change to the width of your default share image
+  shareImageHeight: 600, // Change to the height of your default share image
+  siteLogo: '/logos/logo-512.png', // Logo used for SEO, RSS, and App manifest
+  backgroundColor: '#FFFFFF', // Used for Offline Manifest
+  themeColor: '#FD7F7C', // Used for Offline Manifest
+  copyright: 'Copyright © 2018 Shelfie', // Copyright string for the RSS feed
+}
